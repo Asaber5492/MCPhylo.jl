99e797f1d23da7a7e657b0b8c0291062133e34f8
erathorn <erathorn@users.noreply.github.com>
2020-11-11 14:39:02 +0100

    updated distribution

--- BrownianABC.jl
+++ BrownianABC.jl
@@ -1,14 +1,14 @@
 using Distributed
-addprocs(3);
-@everywhere include("./src/MCPhylo.jl")
-@everywhere using .MCPhylo
-@everywhere using Random
-@everywhere using Distances
-@everywhere import Distributions: sampler, _rand!, logpdf, Normal
-@everywhere import Random: GLOBAL_RNG, rand!
+include("./src/MCPhylo.jl")
+using .MCPhylo
+using Random
+using Distances
+import Distributions: sampler, _rand!, logpdf, Normal
+import Random: GLOBAL_RNG, rand!
+using LinearAlgebra
 
-@everywhere include("BrownianDistr.jl")
-@everywhere include("TreeSugg.jl")
+include("BrownianDistr.jl")
+#include("TreeSugg.jl")
 
 mt, df = make_tree_with_data("notebook/data-st-64-110.paps.nex"); # load your own nexus file
 
@@ -36,7 +36,7 @@ my_data = Dict{Symbol, Any}(
 
 
 
-@everywhere function symtreeprob(d::N)::N where N <: GeneralNode
+function symtreeprob(d::N)::N where N <: GeneralNode
   move = rand([:NNI, :Slide, :Swing, :EdgeLength])
   tree = deepcopy(d)
   if move == :NNI
@@ -56,15 +56,33 @@ my_data = Dict{Symbol, Any}(
 end
 
 
+function my_ELARR(Σ::Array{Float64,2}, σ::Array{Float64})
+    n_concs = length(σ)
+    n_leaves = size(Σ, 1)
+    Σ_L_Arr = Array{Float64,3}(undef, n_concs, n_leaves, n_leaves)
+    @inbounds @simd for i in 1:n_concs
+        Σ_L_Arr[i, :, :] .= cholesky(σ[i] .* Σ).L
+    end
+    Σ_L_Arr
+end
+
+function my_muArr(μ::Array{Float64}, n_leaves::Int64)
+    n_residuals = size(μ, 1)
+    reshape(repeat(μ, outer=n_leaves), n_concs, n_leaves)
+end
+
 
 
 model =  Model(
     arrn = Stochastic(2,
-            (μ, σ, Σ) -> BrownianDistr(μ, σ, Σ), false),
+            (μ_Arr, Σ_Arr, latent) -> BrownianDistr(μ_Arr, Σ_Arr, latent), false),
+    μ_Arr = Logical(2, (μ) -> my_muArr(μ.value, my_data[:leaves]), false),
+    Σ_Arr = Logical(3, (σ, Σ) -> my_ELARR(Σ.value, σ.value), false),
     μ = Stochastic(1, (μH, σH) -> Normal(μH, σH), true),
     μH = Stochastic(()->Normal(), true),
     σH = Stochastic(()->Exponential(), true),
     σ = Stochastic(1,(λ) -> Exponential(λ), true),
+    latent = Stochastic(2, ()->Normal(), false),
     λ = Stochastic(() -> Exponential(), true),
     Σ = Logical(2, (mtree) -> MCPhylo.to_covariance(mtree), false),
     mtree = Stochastic(Node(), () -> CompoundDirichlet(1.0,1.0,0.100,1.0), my_data[:nnodes]+1, true),
@@ -77,10 +95,11 @@ inits = [Dict{Symbol, Union{Any, Real}}(
     :residuals => my_data[:residuals],
     :leaves => my_data[:leaves],
     :μ_raw => my_data[:μ_raw],
+    :latent => rand(my_data[:residuals],my_data[:leaves]),
     :Σ => MCPhylo.to_covariance(mt),
     :μH => rand(),
     :σH => rand(),
-    :μ => randn(my_data[:residuals]),
+    :μ => zeros(my_data[:residuals]),
     :λ => rand(),
     :σ => rand(my_data[:residuals]),
     ),
@@ -100,14 +119,16 @@ inits = [Dict{Symbol, Union{Any, Real}}(
         )
     ]
 
-scheme = [ABC(:μ, 0.5, identity, 0.5, kernel=Normal, maxdraw=100, nsim=3, dist=hamming),
-          ABC(:σ, 0.5, identity, 0.5, kernel=Normal, maxdraw=100, nsim=3, dist=hamming),
-          MCPhylo.ABCT(:mtree, 0.5, identity, 0.5, kernel=Normal, proposal=symtreeprob,maxdraw=100, nsim=3, dist=hamming),
-          Slice([:μH, :σH, :λ], 0.05, Univariate)
+scheme = [ABC([:μ,:σ], 1.0, identity, 100, proposal=Normal, kernel=Normal, maxdraw=25, nsim=3, dist=hamming),
+          ABC(:latent, 1.0, identity, 100, proposal=Normal, kernel=Normal, maxdraw=25, nsim=3, dist=hamming),
+          MCPhylo.ABCT(:mtree, 0.5, identity, 100, proposal=symtreeprob,kernel=Normal, maxdraw=100, nsim=3, dist=hamming),
+          #Slice([:μH, :σH, :λ], 0.05, Multivariate)
+          #NUTS([:μH, :σH]),# dtype=:Zygote),
+          NUTS(:λ)
           ];
 
 setsamplers!(model, scheme)
 
-sim = mcmc(model, my_data, inits, 50000, burnin=10000,thin=50, chains=2, trees=true)
+sim = mcmc(model, my_data, inits, 500, burnin=10,thin=5, chains=1, trees=true)
 
 to_file(sim, "ABC_test")
--- BrownianDistr.jl
+++ BrownianDistr.jl
@@ -1,16 +1,32 @@
 mutable struct BrownianDistr <: DiscreteMatrixDistribution
-    μ::Array{Float64,1}
-    σ::Array{Float64,1}
-    Σ::Array{Float64,2}
+    μ_Arr::Array{Float64,2}
+    Σ_L_Arr::Array{Float64,3}
+    latent::Array{Float64,2}
     dim::Tuple{Int64,Int64}
 
-    function BrownianDistr(μ::Array{Float64,1}, σ::Array{Float64,1}, Σ::Array{Float64})
-        new(μ, σ, Σ, (size(Σ,1), size(μ,1)))
+    function BrownianDistr(μ_Arr::Array{Float64,2}, Σ_L_Arr::Array{Float64,3}, latent::Array{Float64,2})
+        new(μ_Arr, Σ_L_Arr, latent, (size(Σ_L_Arr,2), size(Σ_L_Arr,1)))
+    end
+
+    function BrownianDistr(μ::Array{Float64,1}, σ::Array{Float64,1}, Σ::Array{Float64}, latent::Array{Float64,2})
+        n_concs = size(μ,1)
+        n_leaves = size(Σ,1)
+
+        Σ_L_Arr = Array{Float64,3}(undef, n_concs, n_leaves, n_leaves)
+        μ_Arr::Array{Float64,2} = reshape(repeat(μ, outer=n_leaves), n_concs, n_leaves)
+        @inbounds @simd for i in 1:n_concs
+            Σ_L_Arr[i, :, :] .= cholesky(σ[i] .* Σ).L
+        end
+        new(μ_Arr, Σ_L_Arr, latent, (size(Σ,1), n_concs))
     end
 end
 
-function BrownianDistr(μ::ArrayVariate, σ::ArrayVariate, Σ::ArrayVariate)
-    BrownianDistr(μ.value, σ.value, Σ.value)
+function BrownianDistr(μ::ArrayVariate, σ::ArrayVariate, Σ::ArrayVariate, latent::ArrayVariate)
+    BrownianDistr(μ.value, σ.value, Σ.value, latent.value)
+end
+
+function BrownianDistr(μ_Arr::ArrayVariate, Σ_Arr::ArrayVariate, latent::ArrayVariate)
+    BrownianDistr(μ_Arr.value, Σ_Arr.value, latent.value)
 end
 
 minimum(d::BrownianDistr) = -Inf
@@ -26,9 +42,9 @@ end
 
 function _rand!(r::A, d::BrownianDistr, x::AbstractMatrix) where A <: AbstractRNG
     n_leaves, n_concs = d.dim
-    μ_arr::Array{Float64,2} = reshape(repeat(d.μ, outer=n_leaves), n_concs, n_leaves)
-    @inbounds for i in 1:n_concs
-        x[:,i] .= Int.(invlogit.(rand(MvNormal(μ_arr[i,:], d.σ[i].*d.Σ))) .> 0.5)
+    @inbounds @simd for i in 1:n_concs
+        x[:,i] .= Int.(rand.(Bernoulli.(invlogit.(d.μ_Arr[i,:] + BLAS.gemv('N',1.0,d.Σ_L_Arr[i, :, :], d.latent[i, :])))))
+        #x[:,i] .= Int.(rand.(Bernoulli.(invlogit.(rand(MvNormal(μ_arr[i,:], d.σ[i].*d.Σ))))))
     end
     return x
 end
